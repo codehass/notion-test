1666b55e2bdee36298422a760ec83e73d466a138
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-28 09:31:21 +0100

    create side bad and section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import {
+	MdKeyboardDoubleArrowRight,
+	MdKeyboardDoubleArrowLeft,
+} from "react-icons/md";
+
 function App() {
 	return (
-		<h1 className="text-3xl font-bold underline text-green-500">
-			Hello world!
-		</h1>
+		<div className="flex w-full h-screen">
+			<div className="bg-red-200 h-full flex flex-col w-1/5">
+				<button className="flex justify-end items-center p-3">
+					<MdKeyboardDoubleArrowLeft />
+				</button>
+			</div>
+			<section className="bg-green-200 w-4/5 mx-auto h-full flex flex-col"></section>
+		</div>
 	);
 }
 
