5b91471c2abe0ff20f151f345cb40d46adf6a699
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-28 18:42:04 +0100

    add button to text when we hover it

--- src/components/TextElement.js
+++ src/components/TextElement.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { PiDotsSixVerticalBold } from "react-icons/pi";
 
 function TextElement({ el, index }) {
 	return el.tag === "h1" ? (
-		<h1 className="text-2xl font-bold font-mono" key={index}>
-			{el.content}
-		</h1>
+		<div className="group flex items-center relative">
+			<div className="hidden group-hover:block absolute left-[-15px]">
+				<PiDotsSixVerticalBold className="text-gray-600 text-lg cursor-pointer" />
+			</div>
+			<h1 className="text-2xl font-bold font-mono pl-1" key={index}>
+				{el.content}
+			</h1>
+		</div>
 	) : (
-		<p className="font-mono" key={index}>
-			{el.content}
-		</p>
+		<div className="group flex items-center relative">
+			<div className="hidden group-hover:block absolute left-[-15px]">
+				<PiDotsSixVerticalBold className="text-gray-600 text-lg cursor-pointer" />
+			</div>
+			<p className="font-mono pl-1" key={index}>
+				{el.content}
+			</p>
+		</div>
 	);
 }
 
