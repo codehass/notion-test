fdf41cc72f0b044d52c95de1cbd990f683102c38
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-28 09:38:44 +0100

    add open and close sidebar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
+import { useState } from "react";
+import { BsBook } from "react-icons/bs";
+import { FaChevronDown } from "react-icons/fa6";
+import { FiUnlock } from "react-icons/fi";
 import {
 	MdKeyboardDoubleArrowRight,
 	MdKeyboardDoubleArrowLeft,
 } from "react-icons/md";
-import { BsBook } from "react-icons/bs";
-import { FaChevronDown } from "react-icons/fa6";
-import { FiUnlock } from "react-icons/fi";
 
 function App() {
+	const [sideBarOpen, setSideBarOpen] = useState(true);
+
+	const handleSideBase = () => {
+		setSideBarOpen(!sideBarOpen);
+	};
+
 	return (
 		<div className="flex w-full h-screen">
-			<div className="bg-red-200 h-full flex flex-col w-1/5">
-				<button className="flex justify-end items-center p-3">
-					<MdKeyboardDoubleArrowLeft />
-				</button>
-				<div>Side bar content</div>
-			</div>
-			<section className="w-4/5 mx-auto h-full flex flex-col">
+			{
+				<div
+					className={`bg-red-200 ${
+						sideBarOpen ? "w-1/5" : "w-10"
+					} h-full flex flex-col`}
+				>
+					<button className="flex justify-end items-center p-3">
+						<button type="button" onClick={handleSideBase}>
+							{sideBarOpen ? (
+								<MdKeyboardDoubleArrowLeft />
+							) : (
+								<MdKeyboardDoubleArrowRight />
+							)}
+						</button>
+					</button>
+					{sideBarOpen && <div>Content</div>}
+				</div>
+			}
+
+			<section
+				className={`${
+					sideBarOpen ? "w-4/5" : "w-[90%] mx-auto"
+				} h-full flex flex-col gap-4`}
+			>
 				<header className="flex justify-between p-2">
 					<div className="flex justify-center items-center gap-1">
 						<BsBook />
