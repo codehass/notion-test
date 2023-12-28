8d7b151c1b4800bc2b96f0710333bf1c03ea3ed6
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-28 20:05:03 +0100

    add some changes to side bar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,20 +7,33 @@ import {
 function SideBar({ sideBarOpen, handleSideBase }) {
 	return (
 		<div
-			className={`bg-red-200 ${
-				sideBarOpen ? "w-1/5" : "w-10"
-			} h-full flex flex-col`}
+			className={`bg-[#fbfbfa] ${
+				sideBarOpen ? "w-1/5 h-full" : "w-16"
+			}  flex flex-col`}
 		>
-			<div className="flex justify-end items-center p-3">
+			<div className="flex justify-between items-center p-3 hover:bg-[#EBEBEA] rounded-md">
+				<div className="flex items-center gap-1 ">
+					<span className="bg-gray-300 px-1 rounded-sm text-sm">H</span>
+					<p
+						className={` ${
+							sideBarOpen ? "" : "hidden"
+						} text-sm font-semibold cursor-pointer
+						`}
+					>
+						Hassan EL OUARDY
+					</p>
+				</div>
 				<button type="button" onClick={handleSideBase}>
 					{sideBarOpen ? (
-						<MdKeyboardDoubleArrowLeft />
+						<MdKeyboardDoubleArrowLeft className="text-2xl hover:bg-gray-300 px-1 rounded-sm" />
 					) : (
-						<MdKeyboardDoubleArrowRight />
+						<MdKeyboardDoubleArrowRight className="text-2xl hover:bg-gray-300 px-1 rounded-sm" />
 					)}
 				</button>
 			</div>
-			{sideBarOpen && <div>Content</div>}
+			{sideBarOpen && (
+				<div className="flex justify-center m-auto">Side bar Content</div>
+			)}
 		</div>
 	);
 }
