c10dfea2676156cd356a345ac08b830eb827bb6e
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-28 20:13:59 +0100

    Create edit popup

---
+++ src/components/EditPopup.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function EditPopup({ props }) {
+	const { handleChangeToH1, handleDelete } = props;
+	return (
+		<div className="flex flex-col absolute left-[-120px] bg-white border border-gray-300 space-y-2 shadow-md rounded-md py-3 z-10">
+			<button
+				className="cursor-pointer hover:bg-gray-100 py-1 px-2 text-sm"
+				onClick={handleChangeToH1}
+			>
+				Change to H1
+			</button>
+			<button
+				className="cursor-pointer text-start hover:bg-gray-100 py-1 px-2 text-sm w-full"
+				onClick={handleDelete}
+			>
+				Delete
+			</button>
+		</div>
+	);
+}
+
+export default EditPopup;
--- src/components/TextElement.js
+++ src/components/TextElement.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { PiDotsSixVerticalBold } from "react-icons/pi";
+import EditPopup from "./EditPopup";
 
-function TextElement({ el, index }) {
+function TextElement({ el, index, onDelete, onTagChange }) {
+	const [popupOpen, setPopupOpen] = useState(false);
+
+	const handleEditPopup = () => {
+		setPopupOpen(!popupOpen);
+	};
+
+	const handleDelete = () => {
+		onDelete(index);
+	};
+
+	const handleChangeToH1 = () => {
+		onTagChange(index, "h1");
+		setPopupOpen(false);
+	};
+
+	const props = { handleChangeToH1, handleDelete };
+
 	return el.tag === "h1" ? (
 		<div className="group flex items-center relative">
+			{popupOpen && <EditPopup props={props} />}
 			<div className="hidden group-hover:block absolute left-[-15px]">
-				<PiDotsSixVerticalBold className="text-gray-600 text-lg cursor-pointer" />
+				<PiDotsSixVerticalBold
+					className="text-gray-600 text-lg cursor-pointer"
+					onClick={handleEditPopup}
+				/>
 			</div>
 			<h1 className="text-2xl font-bold font-mono pl-1" key={index}>
 				{el.content}
@@ -13,8 +35,12 @@ function TextElement({ el, index }) {
 		</div>
 	) : (
 		<div className="group flex items-center relative">
+			{popupOpen && <EditPopup props={props} />}
 			<div className="hidden group-hover:block absolute left-[-15px]">
-				<PiDotsSixVerticalBold className="text-gray-600 text-lg cursor-pointer" />
+				<PiDotsSixVerticalBold
+					className="text-gray-600 text-lg cursor-pointer"
+					onClick={handleEditPopup}
+				/>
 			</div>
 			<p className="font-mono pl-1" key={index}>
 				{el.content}
