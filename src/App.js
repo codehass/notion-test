c1048c903d37e717d0f0b72cc21bfeb42bb4b3d9
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-28 09:35:36 +0100

    create the header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import {
 	MdKeyboardDoubleArrowRight,
 	MdKeyboardDoubleArrowLeft,
 } from "react-icons/md";
+import { BsBook } from "react-icons/bs";
+import { FaChevronDown } from "react-icons/fa6";
+import { FiUnlock } from "react-icons/fi";
 
 function App() {
 	return (
@@ -10,8 +13,46 @@ function App() {
 				<button className="flex justify-end items-center p-3">
 					<MdKeyboardDoubleArrowLeft />
 				</button>
+				<div>Side bar content</div>
 			</div>
-			<section className="bg-green-200 w-4/5 mx-auto h-full flex flex-col"></section>
+			<section className="w-4/5 mx-auto h-full flex flex-col">
+				<header className="flex justify-between p-2">
+					<div className="flex justify-center items-center gap-1">
+						<BsBook />
+						<div>
+							<a
+								href="google.com"
+								className="text-gray-500 hover:text-black font-medium"
+							>
+								Main /{" "}
+							</a>
+							<a
+								href="google.com"
+								className="text-gray-500 hover:text-black font-medium"
+							>
+								Getting started /{" "}
+							</a>
+							<a
+								href="google.com"
+								className="text-gray-500 hover:text-black font-medium"
+							>
+								Front-end developer test ...{" "}
+							</a>
+						</div>
+					</div>
+					<div className="flex justify-between gap-1">
+						<div className="flex items-center gap-1 text-gray-500 font-medium">
+							<FiUnlock />
+							<p>Editing</p>
+						</div>
+						<span>|</span>
+						<div className="text-blue-500 flex items-center gap-1">
+							<p className="font-bold">Publish Space </p>
+							<FaChevronDown />{" "}
+						</div>
+					</div>
+				</header>
+			</section>
 		</div>
 	);
 }
