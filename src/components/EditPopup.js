629af4c051651668bc85497a7dd17bac8f6a7c34
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-28 21:42:55 +0100

    fix responsiveness errors

--- src/components/EditPopup.js
+++ src/components/EditPopup.js
@@ -3,7 +3,7 @@ import React from "react";
 function EditPopup({ props }) {
 	const { handleChangeToH1, handleDelete } = props;
 	return (
-		<div className="flex flex-col absolute left-[-120px] bg-white border border-gray-300 space-y-2 shadow-md rounded-md py-3 z-10">
+		<div className="flex flex-col absolute top-[30px] left-[-10px] md:left-[-120px] bg-white border border-gray-300 space-y-2 shadow-md rounded-md py-3 z-10">
 			<button
 				className="cursor-pointer hover:bg-gray-100 py-1 px-2 text-sm"
 				onClick={handleChangeToH1}
