e4a44938b9a179e29b409790f4e7117b6b841217
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-30 13:30:06 +0100

    handle popup to switch between text and h1

--- src/components/EditPopup.js
+++ src/components/EditPopup.js
@@ -1,18 +1,19 @@
 import React from "react";
 
-function EditPopup({ props }) {
-	const { handleChangeToH1, handleDelete } = props;
+function EditPopup({ props, el }) {
+	const { tag, id } = el;
+	const { handleChangeTag, handleDelete1 } = props;
 	return (
 		<div className="flex flex-col absolute top-[30px] left-[-10px] md:left-[-120px] bg-white border border-gray-300 space-y-2 shadow-md rounded-md py-3 z-10">
 			<button
 				className="cursor-pointer hover:bg-gray-100 py-1 px-2 text-sm"
-				onClick={handleChangeToH1}
+				onClick={handleChangeTag}
 			>
-				Change to H1
+				Change to {tag === "h1" ? "Text" : "H1"}
 			</button>
 			<button
 				className="cursor-pointer text-start hover:bg-gray-100 py-1 px-2 text-sm w-full"
-				onClick={handleDelete}
+				onClick={() => handleDelete1(id)}
 			>
 				Delete
 			</button>
